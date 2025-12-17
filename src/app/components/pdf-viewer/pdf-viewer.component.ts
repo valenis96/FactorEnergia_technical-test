f8c9b856f09e33e48186021ebe396e6b77b1c1ed
valenis96 <valentina@maronese.com>
2025-12-17 14:01:42 +0100

    Add a basic pdf viewer

--- src/app/components/pdf-viewer/pdf-viewer.component.ts
+++ src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxExtendedPdfViewerModule, NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-pdf-viewer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxExtendedPdfViewerModule],
   templateUrl: './pdf-viewer.component.html',
-  styleUrls: ['./pdf-viewer.component.scss']
+  styleUrls: ['./pdf-viewer.component.scss'],
+  providers: [NgxExtendedPdfViewerService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PdfViewerComponent {
+
+  constructor(private pdfService: NgxExtendedPdfViewerService) { }
 
 }
