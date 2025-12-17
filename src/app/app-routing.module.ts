d948f5a2ea47035bc01409a04c916b1ba914af40
valenis96 <valentina@maronese.com>
2025-12-17 13:28:42 +0100

    Create pdfviewer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { PdfViewerComponent } from './components/pdf-viewer/pdf-viewer.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent }, { path: 'pdfviewer', component: LoginComponent }];
+const routes: Routes = [{ path: '', component: LoginComponent }, { path: 'pdfviewer', component: PdfViewerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pdf-viewer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pdf-viewer.component.html',
+  styleUrls: ['./pdf-viewer.component.scss']
+})
+export class PdfViewerComponent {
+
+}
