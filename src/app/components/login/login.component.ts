3e7518a4c8d19d14db4a60950688fdddab784b70
valenis96 <valentina@maronese.com>
2025-12-17 13:11:27 +0100

    Basics of login: done

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, FormsModule, MatCardModule, MatButtonModule,],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideAnimations()],
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  loginForm = this.fb.group({
+    email: [null, Validators.required],
+    password: [null, Validators.required],
+  });
+
+
+  constructor(private router: Router,) { }
+
+
+  submitForm(): void {
+    this.router.navigate([`/pdfviewer`]);
+  }
+}
